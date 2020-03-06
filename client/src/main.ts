4b9525e8762e17642ce608a366842d86fefef401
Jonathan Ling <jonathan.ling@hoteleffectiveness.com>
2020-03-06 11:22:32 -0500

    Update main file to remove custom plugin.

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,12 +1,10 @@
 import Vue from 'vue';
-import App from './App.vue';
+import App from '@/App.vue';
 import router from './router';
 import store from './store';
-import HttpServicePlugin, { HttpService } from '@/lib/http.service';
 import './assets/styles/index.css';
 
 Vue.config.productionTip = false;
-Vue.use(HttpServicePlugin);
 
 new Vue({
   router,
